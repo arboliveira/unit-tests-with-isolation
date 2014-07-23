157480ac28cd88a0bddfea43ac05203c8ac0f24e
André de Oliveira <andre.oliveira@liferay.com>
2014-07-23 19:11:05 -0300

    Test local sender. Takes a whopping 10 seconds. #FAIL

--- src/test/java/com/liferay/arbo/massmailing/MassMailingServiceTest.java
+++ src/test/java/com/liferay/arbo/massmailing/MassMailingServiceTest.java
@@ -1,12 +1,21 @@
 package com.liferay.arbo.massmailing;
 
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.support.membermodification.MemberModifier.stub;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.liferay.arbo.email.Address;
+import com.liferay.arbo.email.Message;
 import com.liferay.arbo.global.GlobalSystemParameterConfigurationSettings;
 
 @RunWith(PowerMockRunner.class)
@@ -15,11 +24,23 @@ public class MassMailingServiceTest
 {
 
 	@Test
-	public void smoke()
+	public void local()
 	{
-		mockStatic(GlobalSystemParameterConfigurationSettings.class);
+		mockStatic(
+				GlobalSystemParameterConfigurationSettings.class,
+				CALLS_REAL_METHODS);
+		stub(method(
+				GlobalSystemParameterConfigurationSettings.class,
+				"getMassMailingTargetCountLocalLimit"
+				))
+				.toReturn(3);
 
-		new MassMailingService();
+		Message message = mock(Message.class);
+		Address address1 = mock(Address.class), address2 = mock(Address.class);
+
+		List<Address> targets = Arrays.asList(address1, address2);
+
+		new MassMailingService().send(message, targets);
 	}
 
 }
