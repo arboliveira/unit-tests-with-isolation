4dd8b05fa0b906621dc21052ff41f78e51609ed0
André de Oliveira <andre.oliveira@liferay.com>
2014-07-23 18:10:11 -0300

    PowerMock to the rescue

--- src/test/java/com/liferay/arbo/massmailing/MassMailingServiceTest.java
+++ src/test/java/com/liferay/arbo/massmailing/MassMailingServiceTest.java
@@ -1,13 +1,24 @@
 package com.liferay.arbo.massmailing;
 
-import org.junit.Test;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.liferay.arbo.global.GlobalSystemParameterConfigurationSettings;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ GlobalSystemParameterConfigurationSettings.class })
 public class MassMailingServiceTest
 {
 
 	@Test
 	public void smoke()
 	{
+		mockStatic(GlobalSystemParameterConfigurationSettings.class);
+
 		new MassMailingService();
 	}
 
