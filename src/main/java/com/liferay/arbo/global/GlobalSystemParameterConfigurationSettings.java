9d98c033ec8afbb370f49497398d43f7ef91581e
André de Oliveira <andre.oliveira@liferay.com>
2014-07-24 10:54:34 -0300

    Old tests green again. Test for the new feature still failing

--- src/main/java/com/liferay/arbo/global/GlobalSystemParameterConfigurationSettings.java
+++ src/main/java/com/liferay/arbo/global/GlobalSystemParameterConfigurationSettings.java
@@ -5,7 +5,17 @@ public class GlobalSystemParameterConfigurationSettings
 
 	public static long getMassMailingTargetCountLocalLimit()
 	{
-		throw new ConnectionException(
+		throw requireDatabaseConnection();
+	}
+
+	public static int getMassMailingLineCountLocalLimit()
+	{
+		throw requireDatabaseConnection();
+	}
+
+	private static ConnectionException requireDatabaseConnection()
+	{
+		return new ConnectionException(
 				"You must be connected to the database"
 						+ " to perform this operation!!");
 	}
